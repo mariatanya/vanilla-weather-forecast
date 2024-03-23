f636db1ea92d476649ef289bd695b5e141850efb
mariatanya <151530541+mariatanya@users.noreply.github.com>
2024-03-23 21:15:30 +0200

    Adjusted the main CSS

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ function getForecast(city) {
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
 
   return days[date.getDay()];
 }
